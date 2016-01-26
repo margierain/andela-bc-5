58c12c158610abf5e5c28e26825d13de47dcf131
margierain <margaretrain.mo@gmail.com>
2016-01-26 15:05:29 +0300

    add new reverse function

--- str_reverse.py
+++ str_reverse.py
@@ -4,3 +4,13 @@ def reverse(s):
 #example
 words = "andela"
 print reverse(words)	
+
+def reverse(s):
+	new_s =list(s)
+	for i in range(len(new_s)//2):
+		new_s[i], new_s-i-1 = new_s(new_s)-i-1], new_s[i]
+
+'''		temp = new_s[i]
+		new_s[i] = new_s [len(new_s)-i-1]
+		new_s[len{new_s-i-1] = temp '''
+
